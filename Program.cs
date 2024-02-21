693e8a2d28564dcf0c21efc6182259ae1ca3dab5
SvetlomirChalakov <finndips279@gmail.com>
2024-02-21 08:37:51 +0200

    Lists

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿namespace ListCreate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type a LIST of colors> Done to end: ");
+            List<string> colorList = new List<string>();
+            string input = Console.ReadLine();
+
+            while (input != "Done")
+            {
+                //mushhhh in colorlist
+                colorList.Add(input);
+                input = Console.ReadLine();
+            }
+            colorList[0] = "Pink";
+
+            Console.WriteLine("What element will you delete?(0-" + (colorList.Count - 1) + " ? ");
+            int index = int.Parse(Console.ReadLine());
+
+            colorList.RemoveAt(index);
+
+
+
+            foreach (string color in colorList)
+            {
+                Console.WriteLine(color);
+            }
+
+        }
+    }
+}
